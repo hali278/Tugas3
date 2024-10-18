b078f1b4d547a73d41542e4edbef1df2f3df2a54
Siti Halimah <hlmhi.imah0@gmail.com>
2024-10-18 15:26:49 +0700

    first

---
+++ Enkripsi_Text_Editor/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBuka = New System.Windows.Forms.Button()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btnCari = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCari = New System.Windows.Forms.TextBox()
+        Me.txtIsi = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnBuka
+        '
+        Me.btnBuka.Location = New System.Drawing.Point(15, 9)
+        Me.btnBuka.Name = "btnBuka"
+        Me.btnBuka.Size = New System.Drawing.Size(51, 20)
+        Me.btnBuka.TabIndex = 0
+        Me.btnBuka.Text = "Buka"
+        Me.btnBuka.UseVisualStyleBackColor = True
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.Location = New System.Drawing.Point(86, 9)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(60, 20)
+        Me.btnSimpan.TabIndex = 1
+        Me.btnSimpan.Text = "Simpan"
+        Me.btnSimpan.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(163, 9)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(57, 19)
+        Me.btnHapus.TabIndex = 2
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'btnCari
+        '
+        Me.btnCari.Location = New System.Drawing.Point(390, 9)
+        Me.btnCari.Name = "btnCari"
+        Me.btnCari.Size = New System.Drawing.Size(67, 19)
+        Me.btnCari.TabIndex = 3
+        Me.btnCari.Text = "Cari"
+        Me.btnCari.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(242, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Cari :"
+        '
+        'txtCari
+        '
+        Me.txtCari.Location = New System.Drawing.Point(287, 8)
+        Me.txtCari.Name = "txtCari"
+        Me.txtCari.Size = New System.Drawing.Size(88, 20)
+        Me.txtCari.TabIndex = 5
+        '
+        'txtIsi
+        '
+        Me.txtIsi.Location = New System.Drawing.Point(12, 55)
+        Me.txtIsi.Multiline = True
+        Me.txtIsi.Name = "txtIsi"
+        Me.txtIsi.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtIsi.Size = New System.Drawing.Size(503, 214)
+        Me.txtIsi.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(569, 281)
+        Me.Controls.Add(Me.txtIsi)
+        Me.Controls.Add(Me.txtCari)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCari)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnSimpan)
+        Me.Controls.Add(Me.btnBuka)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnBuka As System.Windows.Forms.Button
+    Friend WithEvents btnSimpan As System.Windows.Forms.Button
+    Friend WithEvents btnHapus As System.Windows.Forms.Button
+    Friend WithEvents btnCari As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtCari As System.Windows.Forms.TextBox
+    Friend WithEvents txtIsi As System.Windows.Forms.TextBox
+
+End Class
---
+++ Enkripsi_Text_Editor/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Function Transform(ByVal text As String) As String
+        Dim result As New System.Text.StringBuilder()
+        For Each c As Char In text
+            ' Geser semua karakter maju 3
+            result.Append(Chr(Asc(c) + 3))
+        Next
+        Return result.ToString()
+    End Function
+
+    Private Sub btnBuka_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnBuka.Click
+        Using dialog As New OpenFileDialog() With {.Filter = "Text Files (*.txt)|*.txt"}
+            If dialog.ShowDialog() = DialogResult.OK Then txtIsi.Text = File.ReadAllText(dialog.FileName)
+        End Using
+    End Sub
+
+    Private Sub btnSimpan_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnSimpan.Click
+        Using dialog As New SaveFileDialog() With {.Filter = "Text Files (*.txt)|*.txt"}
+            If dialog.ShowDialog() = DialogResult.OK Then File.WriteAllText(dialog.FileName, Transform(txtIsi.Text))
+        End Using
+    End Sub
+
+    Private Sub btnHapus_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnHapus.Click
+        If File.Exists(txtCari.Text) Then
+            File.Delete(txtCari.Text)
+            MessageBox.Show("File berhasil dihapus.")
+        Else
+            MessageBox.Show("File tidak ditemukan.")
+        End If
+    End Sub
+
+    Private Sub btnCari_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnCari.Click
+        If File.Exists(txtCari.Text) Then
+            txtIsi.Text = File.ReadAllText(txtCari.Text)
+        Else
+            MessageBox.Show("File tidak ditemukan.")
+        End If
+    End Sub
+End Class
